69a7b50d5b20ddcc525f68b88af46d44f834cad4
Greg Nelson <gregory.nelson@maine.edu>
2025-11-13 14:15:37 -0500

    Totally innocent commit

---
+++ skillshow/src/__tests__/Helpful.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import {collection, getDocs, where, query, doc, deleteDoc} from 'firebase/firestore';
+import AddToDBButton from "../components/AddtoDBButton";
+import { db } from '../firebase-config';
+
+
+describe("Just making sure the tests work", () =>{
+    test("Expert works", () => {
+        render(<AddToDBButton/>);
+        const buttonElement = screen.getByText(/Not there button/i);
+        expect(buttonElement).toBeInTheDocument();
+    });
+
+})
