3bfa8b60b89080f64bada563f67366c054085ddf
Josh VanSantvoord <josh.vansantvoord@maine.edu>
2025-11-22 11:55:40 -0500

    shows loading message test passing

--- skillshow/src/components/DisplayRepos.tsx
+++ skillshow/src/components/DisplayRepos.tsx
@@ -1,15 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { auth, db } from '../firebase-config';
 import { doc, getDoc } from 'firebase/firestore';
 import { GitHubRepoService, Repository } from '../services/GitHubRepoService';
 import { ConnectGitHub } from './ConnectGitHub';
 
+
+const debug = 0;
+
 export function DisplayRepos(){
 
+    const [repos, setRepos] = useState<Repository[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [isConnected, setIsConnected] = useState(false);
+    const [error, setError] = useState("");
+
+    const GitHubService = new GitHubRepoService();
+
+    const loadRepos = async () => {
+
+        setLoading(true);
+        setError("");
+
+        try{
+
+            const userID = auth.currentUser?.uid;
+            if(!userID){
+
+                setError('Please log in first');
+                setLoading(false);
+                return;
+            }
+            
+            // Get the installattion ID for the user to display repo
+            const userDoc = await getDoc(doc(db,'users', userID));
+            const userData = userDoc.data();
+            if(debug){
+                console.log(userData);
+            }
+
+            const installationID = userData?.installationId;
+            if(!installationID){
+                setIsConnected(false);
+                setLoading(false);
+                setError("Error getting installation ID")
+                return;
+            }
+
+            setIsConnected(true);
+        }
+        catch(err){
+             console.error("Error loading repositories:", err);
+        }
+        
+
+
+    }
 
 
 
-    return <div></div>
+        if (loading){
+            return <div>Loading repositories...</div>
+        }
+
+        if (!isConnected){
+
+            return <div><span>GitHub not connected</span></div>
+        }
 
 
 }
