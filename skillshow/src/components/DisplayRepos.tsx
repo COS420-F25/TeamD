7ba0bf60466b85c24f106121c43a833cbcd30530
Josh VanSantvoord <josh.vansantvoord@maine.edu>
2025-11-22 10:19:28 -0500

    Added base file for displayrepos component

---
+++ skillshow/src/components/DisplayRepos.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { auth, db } from '../firebase-config';
+import { doc, getDoc } from 'firebase/firestore';
+import { GitHubRepoService, Repository } from '../services/GitHubRepoService';
+import { ConnectGitHub } from './ConnectGitHub';
+
+export function DisplayRepos(){
+
+
+
+
+    return <div></div>
+
+
+}
